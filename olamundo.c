451fa6c81a897489b68d5c1ad6fdc4537660769c
Arthur Henrique <123035993+Kuromyuto@users.noreply.github.com>
2026-05-06 10:05:34 +0000

    Aula de switch

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int opcao, regras;
+    int numeroSecreto, palpite;
+
+    printf("Menu Principal\n");
+    printf("1. Iniciar Jogo\n");
+    printf("2. Ver Regras\n");
+    printf("3. Sair\n");
+    printf("Escolha: ");
+    scanf("%d", &opcao);
+
+    switch (opcao){
+        case1:
+        srand(time(0));
+        numeroSecreto = rand() %10;
+        printf("Digite um numero de 0 a 9: ");
+        scanf("%d", &palpite);
+        if(numeroSecreto == palpite){
+        printf("voce acertou!\n");
+        printf("Numero secreto %d\n", numeroSecreto);
+        } else {
+        printf("você errou!\n");
+        printf("Numero secreto %d\n", numeroSecreto);
+        }
+        break;
+
+        case2:
+        printf("Explicação das regras");
+        printf("Digite a opção relacionada as regras do jogo\n");
+        scanf("%d", &regras);
+        switch (regras){
+        }
+        break;
+        case3:
+        printf("Saindo do jogo!\n");
+        break;
+        default:
+        printf("Opção invalida");
+        break;
+    }
+
+}
+
 
