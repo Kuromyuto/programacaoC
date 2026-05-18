a07bff784a099c555e43714497fac3cd6455e922
Arthur Henrique <123035993+Kuromyuto@users.noreply.github.com>
2026-05-18 09:59:11 +0000

    Jogo de Maior, Menor ou Igual

--- calculo.c
+++ calculo.c
@@ -1,13 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int num1 = 40; 
-    int num2 = 20;
-    int maior;
+    int numeroJogador, numeroComputador, resultado;
+    char tipoComparacao;
 
-    num1 > num2 ? (maior = num1) : (maior = num2);
+    //Gerar numero aleatório
+    srand (time(0));
+    numeroComputador = rand () % 100 + 1; // numero entre 1 e 100
 
-    printf("O numero maior é %d\n", maior);
+    //Inicio do jogo
+    printf("Bem vindo ao jogo Maior, Menor ou igual!\n");
+    printf("Você deve escolher um numero e o tipo de comparação\n");
+    printf("M. Maior\n");
+    printf("N. Menor\n");
+    printf("I. Igual\n");
+
+    printf("Escolha a comparação: ");
+    scanf("%c", &tipoComparacao);
+
+    printf("Digite seu numero (entre 1 e 100): ");
+    scanf("%d", &numeroJogador);
+
+    //Exibir numero do computador
+    // printf("O numero do computador é %d\n", numeroComputador);
+
+    switch (tipoComparacao)
+    {
+    case 'M':
+    case 'm':
+        printf("Você escolheu a opção maior!\n");
+        resultado = numeroJogador > numeroComputador ? 1 : 0;
+        break;
+    case 'N':
+    case 'n':
+        printf("Você escolheu a opção menor!\n");
+        resultado = numeroJogador < numeroComputador ? 1:0;
+    break;
+    case 'I':
+    case 'i':
+        printf("Você escolheu a opção igual!\n");
+        resultado = numeroJogador == numeroComputador ? 1: 0;
+    break;
+
+    default:
+    printf("Opção de jogo invalida\n");
+        break;
+    }
+
+    printf("O numero do computador é: %d e do jogador é: %d\n", numeroComputador, numeroJogador);
+
+    if (resultado == 1)
+    {
+       printf("Parabéns Você venceu!\n");
+    } else {
+        printf("Infelizmente Você perdeu!\n");
+    }
+    
    
     return 0;
 }
