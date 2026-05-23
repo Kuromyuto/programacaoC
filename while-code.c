ef1874a0a6fcffa9865bfdc27cbb2cc65e6fc806
Arthur Henrique <123035993+Kuromyuto@users.noreply.github.com>
2026-05-23 14:07:46 +0000

    Fixando estruturas de repetição

---
+++ while-code.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main(){
+
+    // CALCULADORA
+    
+    // int numero, i;
+
+    // printf("Digite um numero para calcularmos a tabuada...\n");
+    // scanf("%d", &numero);
+
+    // for (i = 0; i <= 10; i++)
+    // {
+    //   printf("%d x %d = %d \n", numero, i, i * numero);
+    // }
+
+    //   return 0;
+    
+
+    // TESTE PAR OU IMPAR
+    
+    // int numero;
+    
+    // do {
+    // printf("Digite um numero par para sair do programa...\n");
+    // scanf("%d", &numero);
+
+    // if(numero % 2 == 0){
+    //     printf("%d é par!\n", numero);
+    // } else {
+    //     printf("%d é impar!\n", numero);
+    // }
+
+    // }while(numero % 2 != 0);
+    
+    // printf("Você digitou um numero par, saindo do programa...\n");
+
+
+
+    // while(i <= 10){
+    
+    // if(i % 2 == 0){
+
+
+    //     printf("O numero %d é par\n", i)
+    // //     printf("O numero %d é par\n", i);
+
+    // // } else if (i % 2 != 0) {
+
+    // //     printf("O numero %d é impar\n", i);
+
+    // }
+
+    // i++;
+
+    }
+
+    
